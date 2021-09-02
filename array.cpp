a5bc727f630deeac7d3efa9b8cf0ecf986d42c40
Sukhvsin2 <46042060+Sukhvsin2@users.noreply.github.com>
2021-09-02 13:09:08 -0500

    Add files via upload

--- array.cpp
+++ array.cpp
@@ -3,8 +3,20 @@
 using namespace std;
 
 int main(){
-    int *arr = nullptr;
-    cout<<&arr;
+    int s;
+    cout<<"How many integers: ";
+    cin>>s;
+
+    int *arr = new int[s];
+
+    for(int i=0;i<s;i++){
+        arr[i] = i + 1;
+    }
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    delete arr;
 
     return 0;
 }
