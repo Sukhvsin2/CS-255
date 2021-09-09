b799174a41a8df4a810bda5f62490645f7b18c29
Sukhvsin2 <46042060+Sukhvsin2@users.noreply.github.com>
2021-09-09 13:07:31 -0500

    Add files via upload

--- classes.cpp
+++ classes.cpp
@@ -5,12 +5,29 @@ using namespace std;
 class MyClass{
     private:
         char name[25];
+        int num, den;
     public:
         void setName(){
             cin>>name;
         }
         void getName(){
-            cout<<name;
+            cout<<name<<endl;
+        }
+
+        MyClass(){
+            num = 1;
+            den = 1;
+        }
+
+        void setFraction(){
+            cout<<"NUM: ";
+            cin>>num;
+            cout<<"DEN: ";
+            cin>>den;
+        }
+
+        void getFraction(){
+            cout<<"Fraction: "<<num<<"/"<<den<<endl;
         }
 
 };
@@ -19,5 +36,8 @@ int main(){
     MyClass c1;
     c1.setName();
     c1.getName();
+
+    c1.setFraction();
+    c1.getFraction();
     return 0;
 }
