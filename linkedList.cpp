7a0b105505bc689913ff8a85a525aecbf5174d97
Sukhvsin2 <46042060+Sukhvsin2@users.noreply.github.com>
2021-10-21 12:52:33 -0500

    LinkedList created

---
+++ linkedList.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* ptr;
+        Node(int data){
+            this->data = data;
+            ptr = nullptr;
+        }
+};
+
+class LinkedList{
+    Node *head;
+    public:
+        LinkedList(){
+            head = nullptr;
+        }
+        void ins(int d){
+            Node *newNode = new Node(d);
+            if(head == nullptr){
+                head = newNode;
+                return;
+            }
+            Node *temp = head;
+            while(temp->ptr != nullptr){
+                temp = temp->ptr;
+            }
+            temp->ptr = newNode;
+        }
+        void print(){
+            Node *temp = head;
+            while(temp != nullptr){
+                cout<<temp->data<<" ";
+                temp = temp->ptr;
+            }
+        }
+};
+
+int main(){
+    LinkedList l1;
+    l1.ins(10);
+    l1.ins(20);
+    l1.ins(30);
+    l1.print();
+    return 0;
+}
